e5f57e769cf567c9fe36789df3328126ad393f31
Jan Machacek <janmachacek097@gmail.com>
2023-09-24 14:41:31 +0200

    Simple repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Write;
+
+type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
+
+fn main() -> Result<()> {
+    loop {
+        let mut input = String::new();
+        print!("squil> ");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut input)?;
+        match input.trim() {
+            ".exit" => break,
+            ".help" => println!("I need help too :("),
+            _ => println!("Unrecognized command '{}'.", input.trim()),
+        }
+        // println!("{}", input);
+    }
+
+    Ok(())
 }
+
+
